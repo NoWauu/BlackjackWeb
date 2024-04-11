1f3f62f4b6038105c884ceef977e562c18946c3e
NoWay_y <74825224+NoWauu@users.noreply.github.com>
2024-04-11 11:23:29 +0200

    tirage de cartes ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const joueurs = {
         pointsMain: 0,
         pointsPartie: 0
     },
-    "Joueur 1": {
+    "player1": {
         main: [],
         pointsMain: 0,
         pointsPartie: 0
@@ -17,60 +17,77 @@ const joueurs = {
 
 
 // Création du paquet de carte
-const cards = [];
-for(const color of ["pique", "coeur", "carreau", "trefle"]) {
-    for (let i=0; i<=12; ++i) {
-        cards.push(`${integerToWord(i)}-${color}`);
+function fillDeck() {
+    let cards = [];
+    for(const color of ["spades", "hearts", "diamonds", "clubs"]) {
+        for (let i=0; i<=12; ++i) {
+            cards.push(`${integerToWord(i)}-${color}`);
+        }
     }
+    return cards;
 }
+
+let cards = fillDeck();
+
 
 function integerToWord(num) {
     switch(num) {
         case 0:
-            return "2-deux";
+            return "2-two";
         case 1:
-            return "3-trois";
+            return "3-three";
         case 2:
-            return "4-quatre";
+            return "4-four";
         case 3:
-            return "5-cinq";
+            return "5-five";
         case 4:
             return "6-six";
         case 5:
-            return "7-sept";
+            return "7-seven";
         case 6:
-            return "8-huit";
+            return "8-eight";
         case 7:
-            return "9-neuf";
+            return "9-nine";
         case 8:
-            return "10-dix";
+            return "10-ten";
         case 9:
-            return "10-valet";
+            return "10-jack";
         case 10:
-            return "10-dame";
+            return "10-queen";
         case 11:
-            return "10-roi";
+            return "10-king";
         case 12:
-            return "11-as";
+            return "11-ace";
         default:
             return "";
     }
 }
+
+function getCardImg(cardName) {
+    let c = cardName.split("-");
+    
+    return `${c[1]}_of_${c[2]}.svg`;
+}
+
 //
 document.getElementById("draw").addEventListener('click', function() {
-    drawCard(joueurs["Joueur 1"]);
-    drawCard(joueurs.banque);
+    drawCard("player1", joueurs["player1"]);
 });
 document.getElementById("start").addEventListener('click', startRound);
 document.getElementById("lock").addEventListener('click', lockCards);
+document.getElementById("draw").style.opacity = 0;
+document.getElementById("draw").disabled = true;
+document.getElementById("lock").style.opacity = 0;
+document.getElementById("lock").disabled = true;
 
-let nbCardsPlayer = -2;
-
-function drawCard(joueur) {
+function drawCard(nom, joueur) {
     if(gameStatus !== "playing") return;
     if(mainBloquee) return;
 
-    let card = Math.floor(Math.random() * 52);
+    let card = Math.floor(Math.random() * cards.length);
+    console.log(card);
+    console.log(cards[card]);
+    console.log(getCardImg(cards[card]));
     joueur.main.push(cards[card]);
     joueur.pointsMain = countPoints(joueur)
 /*
@@ -83,19 +100,23 @@ function drawCard(joueur) {
 
 
     let newCard = document.createElement("img");
-    newCard.src = "images/10_of_clubs.svg";
+    newCard.src = `images/${getCardImg(cards[card])}`;
     newCard.className = "card";
-    newCard.style.rotate = `${nbCardsPlayer*5}deg`;
+    /*if (nom === "banque") {
+        newCard.classList.add("cover");
+    }*/
 
-    document.getElementById("player-hand").append(newCard);
-    
-    nbCardsPlayer++;
+    document.getElementById(nom + `-hand`).append(newCard);
 /*
     if (joueur.pointsMain > 21) {
         console.log("La partie est perdue");
         mainBloquee = true;
     }
-*/
+    */
+    cards.splice(card, 1);
+    if(cards.length == 0){
+        cards = fillDeck();
+    }
 }
 
 function countPoints(joueur) {
@@ -110,6 +131,11 @@ function startRound() {
     gameStatus = "playing";
     mainBloquee = false;
     document.getElementById("start").style.opacity = 0;
+    document.getElementById("start").disabled = true;
+    document.getElementById("draw").style.opacity = 1;
+    document.getElementById("draw").disabled = false;
+    document.getElementById("lock").style.opacity = 1;
+    document.getElementById("lock").disabled = false;
     console.log("Début de la partie");
 
 
@@ -119,8 +145,8 @@ function startRound() {
     });
 
     for(let i = 0; i < 2; ++i) {
-        Object.entries(joueurs).forEach( ([k, _])=> {
-            drawCard(joueurs[k]);
+        Object.entries(joueurs).forEach(([k, _])=> {
+            drawCard(k, joueurs[k]);
         });
     }
 
